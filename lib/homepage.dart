505eac70827943817836b8e7bc877d4beeddfb7f
deeppatel23 <deeppatel2.2312@gmail.com>
2022-01-08 10:14:42 +0530

    connected firebase

---
+++ lib/homepage.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String _appName = "Healthcare";
+  String _appDescription = "";
+  bool showDes = false;
+
+  getAppDetails() async {
+    await FirebaseFirestore.instance
+        .collection('global_details')
+        .doc('app')
+        .get()
+        .then((value) {
+      _appName = value.data()!['name'];
+      _appDescription = value.data()!['description'];
+    });
+  }
+
+  @override
+  void initState() {
+    getAppDetails();
+    showDes = false;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_appName == "" ? "App Name" : _appName),
+      ),
+      body: RefreshIndicator(
+        onRefresh: () async {
+          await getAppDetails();
+          setState(() {});
+        },
+        child: Center(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                TextButton(
+                    onPressed: () {
+                      setState(() {
+                        showDes = !showDes;
+                      });
+                    },
+                    child: const Text("View app description")),
+                showDes == true
+                    ? Text(_appDescription == ""
+                        ? "App Description"
+                        : _appDescription)
+                    : Container(),
+              ]),
+        ),
+      ),
+    );
+  }
+}
